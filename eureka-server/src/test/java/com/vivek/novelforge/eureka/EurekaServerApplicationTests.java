3e4c55dd5c3c415110b2e93a08570e75a1dddf15
Vivek <vivekmakwana9408@gmail.com>
2026-08-30 11:46:10 +0530

    eureka:set up eureka server

--- eureka-server/src/test/java/com/vivek/novelforge/eureka/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/vivek/novelforge/eureka/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.vivek.novelforge.content_service;
+package com.vivek.novelforge.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ContentServiceApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
